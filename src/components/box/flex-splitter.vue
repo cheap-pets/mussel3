21033d0d15261a68e93b95e7b353f82c414812bb
scrollbar-ww <scrollbar@163.com>
2024-06-25 21:14:49 +0800

    ✨: + dblclick function
    undefined

--- src/components/box/flex-splitter.vue
+++ src/components/box/flex-splitter.vue
@@ -1,11 +1,27 @@
 <template>
-  <div ref="el" class="mu-flex-splitter" :direction="direction" @mousedown="onMouseDown" />
+  <div
+    ref="el"
+    class="mu-flex-splitter"
+    :direction="direction"
+    @dblclick="onDblClick"
+    @mousedown="onMouseDown">
+    <slot name="handle">
+      <svg class="mu-flex-splitter_handle" xmlns="http://www.w3.org/2000/svg" :viewBox="svg.viewBox">
+        <path stroke="currentColor" stroke-width="1" :d="svg.d" />
+      </svg>
+    </slot>
+  </div>
 </template>
 
 <script setup>
-  import { ref, computed } from 'vue'
+  import { ref, computed, provide } from 'vue'
 
   const el = ref()
+
+  const props = defineProps({
+    collapseButton: Boolean,
+    collapseThreshold: { type: Number, default: 150 }
+  })
 
   const direction = computed(() => {
     const parent = el.value?.parentNode
@@ -16,11 +32,23 @@
     )
   })
 
+  const svg = computed(() =>
+    direction.value === 'row'
+      ? {
+        viewBox: '0 0 4 23',
+        d: 'M0 1h4M0 4h4M0 7h4M0 10h4M0 13h4M0 16h4M0 19h4M0 22h4'
+      }
+      : {
+        viewBox: '0 0 23 4',
+        d: 'M1 0v4M4 0v4M7 0v4M10 0v4M13 0v4M16 0v4M19 0v4M22 0v4'
+      }
+  )
+
   function isResizableElement (node) {
     const { position, display } = window.getComputedStyle(node)
 
     return (
-      display !== 'none' &&
+      (display !== 'none' || node.classList.contains('mu-flex-collapsed')) &&
       !['fixed', 'absolute', 'sticky'].includes(position) &&
       !node.classList.contains('mu-flex-splitter')
     )
@@ -100,14 +128,17 @@
         }
     }
 
-    const { min: prevMin, max: prevMax, size: prevInitSize } = getComputedStyle(prevSibling)
-    const { min: nextMin, max: nextMax, size: nextInitSize } = getComputedStyle(nextSibling)
+    const { min: prevMinSize, max: prevMaxSize, size: prevInitSize } = getComputedStyle(prevSibling)
+    const { min: nextMinSize, max: nextMaxSize, size: nextInitSize } = getComputedStyle(nextSibling)
 
-    const total = prevInitSize + nextInitSize
-    const minOffset = Math.max(prevMin, nextMax ? total - nextMax : 0) - prevInitSize
-    const maxOffset = Math.min(prevMax || total, total - nextMin) - prevInitSize
+    const totalSize = prevInitSize + nextInitSize
+    const minOffset = Math.max(prevMinSize, nextMaxSize ? totalSize - nextMaxSize : 0) - prevInitSize
+    const maxOffset = Math.min(prevMaxSize || totalSize, totalSize - nextMinSize) - prevInitSize
 
     return {
+      totalSize,
+      prevMinSize,
+      nextMinSize,
       prevInitSize,
       nextInitSize,
       minOffset,
@@ -115,7 +146,25 @@
     }
   }
 
-  function onMouseDown (event) {
+  function isCollapsible (node) {
+    return ![null, 'false'].includes(node.getAttribute('collapsible'))
+  }
+
+  let paramsCache
+  let paramsCacheTimer
+
+  function resetParamsCacheTimer () {
+    if (paramsCacheTimer) clearTimeout(paramsCacheTimer)
+
+    paramsCacheTimer = setTimeout(() => {
+      paramsCache = null
+      paramsCacheTimer = null
+    }, 500)
+  }
+
+  function getResizeParams () {
+    if (paramsCache) return paramsCache
+
     const siblings = getResizeSiblings()
 
     if (!siblings) return
@@ -123,24 +172,101 @@
     resetSiblingsFlexSize()
 
     const { prevSibling, nextSibling } = siblings
+
+    const rangeParams = getResizeRange(prevSibling, nextSibling)
+
+    const { prevMinSize, nextMinSize } = rangeParams
+
+    paramsCache = {
+      prevSibling,
+      nextSibling,
+      prevCollapsible: isCollapsible(prevSibling),
+      nextCollapsible: isCollapsible(nextSibling),
+      ...rangeParams,
+      prevCollapseThreshold:
+        prevMinSize
+          ? prevMinSize - props.collapseThreshold
+          : props.collapseThreshold,
+      nextCollapseThreshold:
+        nextMinSize
+          ? nextMinSize - props.collapseThreshold
+          : props.collapseThreshold
+    }
+
+    resetParamsCacheTimer()
+
+    return paramsCache
+  }
+
+  function onDblClick (event) {
+    const params = getResizeParams()
+
+    if (!params) return
+
+    const {
+      totalSize, minOffset, maxOffset,
+      prevInitSize, nextInitSize,
+      prevSibling, nextSibling
+    } = params
+
+    const offset = Math.trunc(totalSize / 2) - prevInitSize
+
+    if (offset >= minOffset && offset <= maxOffset) {
+      prevSibling.style.flexBasis = `${prevInitSize + offset}px`
+      nextSibling.style.flexBasis = `${nextInitSize - offset}px`
+    }
+  }
+
+  function onMouseDown (event) {
+    const params = getResizeParams()
+
+    if (!params) return
+
     const { pageX: startX, pageY: startY } = event
 
     const {
-      prevInitSize,
-      nextInitSize,
-      minOffset,
-      maxOffset
-    } = getResizeRange(prevSibling, nextSibling)
+      totalSize, minOffset, maxOffset,
+      prevSibling, nextSibling,
+      prevInitSize, nextInitSize,
+      prevCollapsible, nextCollapsible,
+      prevCollapseThreshold, nextCollapseThreshold
+    } = params
+
+    function calcAndCollapse (offset) {
+      const collapseTarget =
+        prevCollapsible && (prevInitSize + offset < prevCollapseThreshold)
+          ? prevSibling
+          : nextCollapsible && (nextInitSize - offset < nextCollapseThreshold)
+            ? nextSibling
+            : null
+
+      if (!collapseTarget) {
+        prevSibling.classList.remove('mu-flex-collapsed')
+        nextSibling.classList.remove('mu-flex-collapsed')
+        return
+      }
+
+      const expandTarget = collapseTarget === prevSibling ? nextSibling : prevSibling
+
+      collapseTarget.classList.add('mu-flex-collapsed')
+      collapseTarget.style.flexBasis = 0
+
+      expandTarget.classList.remove('mu-flex-collapsed')
+      expandTarget.style.flexBasis = `${totalSize}px`
+
+      return true
+    }
 
     function onMouseMove (e) {
-      const offset = direction.value === 'row'
+      let offset = direction.value === 'row'
         ? e.pageX - startX
         : e.pageY - startY
 
-      if (offset < minOffset || offset > maxOffset) return
-
-      prevSibling.style.flexBasis = `${prevInitSize + offset}px`
-      nextSibling.style.flexBasis = `${nextInitSize - offset}px`
+      if (!calcAndCollapse(offset)) {
+        offset = Math.max(minOffset, Math.min(offset, maxOffset))
+        prevSibling.style.flexBasis = `${prevInitSize + offset}px`
+        nextSibling.style.flexBasis = `${nextInitSize - offset}px`
+      }
     }
 
     function onMouseUp () {
@@ -155,16 +281,21 @@
     window.addEventListener('mousemove', onMouseMove)
     window.addEventListener('mouseup', onMouseUp)
   }
+
+  provide('direction', direction)
 </script>
 
 <style>
   .mu-flex-splitter {
     --mu-flex-splitter-size: 2px;
-    --mu-flex-splitter-scale: 2;
+    --mu-flex-splitter-hover-size: 6px;
 
-    --mu-flex-splitter-bg: var(--mu-border-color);
-    --mu-flex-splitter-hover-bg: var(--mu-border-color);
+    --mu-flex-splitter-bg: #555;
+    --mu-flex-splitter-hover-bg: #666;
     --mu-flex-splitter-active-bg: var(--mu-primary-color);
+
+    --mu-flex-splitter-handle-size: calc(var(--mu-flex-splitter-hover-size) - 2px);
+    --mu-flex-splitter-handle-color: #fff;
 
     cursor: col-resize;
     user-select: none;
@@ -175,41 +306,95 @@
     overflow: visible;
     display: none;
     flex: 0 0 var(--mu-flex-splitter-size);
+    align-items: center;
     align-self: stretch;
+    justify-content: center;
 
     &::before {
       content: '';
       position: absolute;
-      inset: 0;
       background: var(--mu-flex-splitter-bg);
     }
 
     &[direction] {
-      display: block;
+      display: flex;
     }
 
-    &[active],
-    &:hover {
-      &::before {
-        transform: scaleX(var(--mu-flex-splitter-scale));
-        background: var(--mu-flex-splitter-hover-bg);
-      }
+    &:hover::before {
+      background: var(--mu-flex-splitter-hover-bg);
     }
 
     &[active]::before {
       background: var(--mu-flex-splitter-active-bg);
     }
 
-    &[draggable="false"] {
+    &:hover,
+    &[active] {
+      z-index: 20;
+
+      & > .mu-flex-splitter_handle {
+        display: block;
+      }
+    }
+
+    &[resizable="false"] {
       pointer-events: none;
+
+      & > .mu-flex-splitter_handle {
+        display: none !important;
+      }
+    }
+  }
+
+  .mu-flex-splitter_handle {
+    position: absolute;
+    display: none;
+    color: var(--mu-flex-splitter-handle-color);
+  }
+
+  .mu-flex-splitter[direction="row"] {
+    cursor: col-resize;
+
+    &::before {
+      top: 0;
+      bottom: 0;
+      width: var(--mu-flex-splitter-size);
+    }
+
+    & > .mu-flex-splitter_handle {
+      width: var(--mu-flex-splitter-handle-size);
+    }
+
+    &:hover,
+    &[active] {
+      &::before {
+        width: var(--mu-flex-splitter-hover-size);
+      }
     }
   }
 
   .mu-flex-splitter[direction="column"] {
     cursor: row-resize;
 
-    &:hover::before {
-      transform: scaleY(var(--mu-flex-splitter-scale));
+    &::before {
+      right: 0;
+      left: 0;
+      height: var(--mu-flex-splitter-size);
     }
+
+    & > .mu-flex-splitter_handle {
+      height: var(--mu-flex-splitter-handle-size);
+    }
+
+    &:hover,
+    &[active] {
+      &::before {
+        height: var(--mu-flex-splitter-hover-size);
+      }
+    }
+  }
+
+  .mu-flex-collapsed {
+    display: none !important;
   }
 </style>
